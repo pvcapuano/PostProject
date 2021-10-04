4d37caf83d02617811f5e5398cd3e22ec02ea8ef
pvcapuano <pvcapuano@gmail.com>
2021-10-04 17:42:12 -0300

    form finished

--- src/components/FormModal/index.jsx
+++ src/components/FormModal/index.jsx
@@ -1,28 +1,41 @@
 import React from "react";
+import { useContext } from "react";
+import { PostContext } from "../../contexts/PostContext";
+
 
 import {
     Overlay, Container, Header, FormContainer, FormMain, InputGroup, Footer, CloseIcon, CheckIcon }
      from './styles'
 
 export default function FormModal(){
+
+    const { handleClose, 
+        title, 
+        titleHandler, 
+        content, 
+        contentHandler, 
+        handleSubmit 
+    } = useContext(PostContext)
+
     return(
         <Overlay>
             <Container>
                 <Header>
                     <strong>Adicione um post</strong>
-                    <button type="button">
+                    <button type="button" onClick={handleClose}>
                         <CloseIcon />
                     </button>
                 </Header>
-                <FormContainer>
+                <FormContainer onSubmit={handleSubmit}>
                     <FormMain>
                         <InputGroup>
                         <label htmlFor="title">Título</label>
-                        <input id="title" type="text"></input>
+                        <input id="title" type="text" value={title} onChange={titleHandler}></input>
                         </InputGroup>
                         <InputGroup>
                         <label htmlFor="content">Postagem</label>
-                        <textarea id="content"></textarea>
+                        <textarea id="content" rows="5" cols="30"
+                            minLength="3" maxLength="200" value={content} onChange={contentHandler}></textarea>
                         </InputGroup>
                     </FormMain>
 
--- src/contexts/PostContext.js
+++ src/contexts/PostContext.js
@@ -1,17 +1,52 @@
 import { createContext, useState } from "react";
-import FormModal from "../FormModal";
+import FormModal from "../components/FormModal";
+
+import api from "../services/api"
 
 export const PostContext = createContext()
 
 export function PostContextProvider({children}) {
     const [openFormModal, setOpenFormModal] = useState(false)
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
 
     function handleAdd() {
         setOpenFormModal(true)
     }
 
+    function handleClose() {
+        setOpenFormModal(false)        
+    }
+
+    function titleHandler(event) {
+        setTitle(event.target.value)
+    }
+
+    function contentHandler(event) {
+        setContent(event.target.value)   
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const post = {
+            title, content
+        }
+        api.post("/feed", post)
+        setOpenFormModal(false)  
+    }
+
     return(
-    <PostContext.Provider value={{handleAdd}}>
+    <PostContext.Provider 
+    value={{handleAdd, 
+        handleClose, 
+        title, 
+        setTitle, 
+        content, 
+        setContent,
+        titleHandler,
+        contentHandler,
+        handleSubmit
+        }}>
             {children}
             {openFormModal && <FormModal />}
         </PostContext.Provider>
