fc4b2bcaf3cf047bc6dc29edce115816d68a0154
pvcapuano <pvcapuano@gmail.com>
2021-10-04 13:34:32 -0300

    test api

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -8,26 +8,27 @@ import api from "../../services/api"
 
 export default function PostList() {
 
-    const [posts, setPosts] = useState([])
+    const [feeds, setFeeds] = useState([])
 
     useEffect(() => {
-        api.get("/feeds").then((response) => {
-            setPosts(response.data)
+        api.get("/feeds", { headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTMsInVzZXJuYW1lIjoibHVpemhycWFzQGdtYWlsLmNvbSIsImlhdCI6MTYzMzM2MTgzNSwiZXhwIjoxNjMzMzY1NDM1fQ.K0Ub5nLxPwg7COka0rKEGez79MdE9WvERTq76F_5E0c'}}).then((response) => {
+            setFeeds(response.data)
+            console.log(response.data)
         })
-        console.log(posts)
+        console.log(feeds)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []) 
 
     return (
         <Container>
             <PostListWrapper>
-                {posts?.map((post) => (
+                {feeds?.map((feed) => (
                     <Post
-                    key={post.authorId}
-                    id={post.authorId}
-                    title={post.title}
-                    description={post.description}
-                    liked={post.liked}
+                    key={feed.id}
+                    id={feed.id}
+                    title={feed.username}
+                    description={feed.content}
+                    liked={feed.likes}
                     />
                 ))}
                 <AddPost />
